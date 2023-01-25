5f0b65669ca07b1bc7a3dcd1a49ec81ac660a2b4
Paco Guevara <pacoguevaraa@gmail.com>
2023-01-25 14:12:37 -0600

    Add loading state

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ async function init () {
   let currentGuess = '';
   let currentRow  = 0;
 
+  const res = await fetch('https://words.dev-apis.com/word-of-the-day');
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  setLoading(false);
+  console.log(word);
+  
   function addLetter (letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       currentGuess += letter;
@@ -43,6 +49,10 @@ async function init () {
 
 function isLetter (value) {
   return /^[a-zA-Z]$/.test(value);
+}
+
+function setLoading (isLoading) {
+  loadingDiv.classList.toggle('hidden', !isLoading);
 }
 
 init();
